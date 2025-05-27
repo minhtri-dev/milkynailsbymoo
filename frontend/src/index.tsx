09980967c3a0ba8bcbbac7e49c70fa642ae3835d
Brian Tran <minhtritran.dev@gmail.com>
2025-05-27 16:00:35 +1000

    Refactor CI workflow: remove test step and updated ESLint config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,20 +5,12 @@ import { Route, Routes, BrowserRouter } from 'react-router'
 import './index.css'
 import * as Pages from './pages'
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Pages.Home />} />
         </Routes>
       </BrowserRouter>
-    </>
-  )
-}
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
   </StrictMode>,
 )
