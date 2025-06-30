81a6b53ca1799aac4ba4428e67874a76f6662c3f
minhtri-dev <minhtritran.dev@gmail.com>
2025-07-01 02:49:00 +1000

    Fixed bug with portfolio not showing up

--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -55,18 +55,18 @@ const ImgWithFallback = ({ src, l }: ImgWithFallbackProps) => {
 }
 
 const images = [
-  { src: './portfolio/client_1.webp', l: 'h-[290px]' },
-  { src: './portfolio/client_2.webp', l: 'h-[290px]' },
-  { src: './portfolio/client_3.webp', l: 'h-[174px]' },
-  { src: './portfolio/client_4.webp', l: 'h-[155px]' },
-  { src: './portfolio/client_5.webp', l: 'h-[349px]' },
-  { src: './portfolio/client_6.webp', l: 'h-[250px]' },
-  { src: './portfolio/client_7.webp', l: 'h-[349px]' },
-  { src: './portfolio/client_8.webp', l: 'h-[155px]' },
-  { src: './portfolio/client_9.webp', l: 'h-[250px]' },
-  { src: './portfolio/client_10.webp', l: 'h-[290px]' },
-  { src: './portfolio/client_11.webp', l: 'h-[155px]' },
-  { src: './portfolio/client_12.webp', l: 'h-[309px]' },
+  { src: '/portfolio/client_1.webp', l: 'h-[290px]' },
+  { src: '/portfolio/client_2.webp', l: 'h-[290px]' },
+  { src: '/portfolio/client_3.webp', l: 'h-[174px]' },
+  { src: '/portfolio/client_4.webp', l: 'h-[155px]' },
+  { src: '/portfolio/client_5.webp', l: 'h-[349px]' },
+  { src: '/portfolio/client_6.webp', l: 'h-[250px]' },
+  { src: '/portfolio/client_7.webp', l: 'h-[349px]' },
+  { src: '/portfolio/client_8.webp', l: 'h-[155px]' },
+  { src: '/portfolio/client_9.webp', l: 'h-[250px]' },
+  { src: '/portfolio/client_10.webp', l: 'h-[290px]' },
+  { src: '/portfolio/client_11.webp', l: 'h-[155px]' },
+  { src: '/portfolio/client_12.webp', l: 'h-[309px]' },
 ]
 
 // TODO: Change to use Instagram photos for images
