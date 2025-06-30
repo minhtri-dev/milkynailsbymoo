4796506c8d183f18f073691860a04b48595b83e8
minhtri-dev <minhtritran.dev@gmail.com>
2025-06-30 20:37:19 +1000

    fixed linting issues / formatted

--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -8,19 +8,67 @@ const Portfolio = () => {
         <h1 className="text-midnight mt-30 mb-20 text-center text-5xl font-bold">
           Portfolio
         </h1>
-        <div className="max-w-140 md:max-w-4xl mx-auto columns-2 md:columns-4 p-4 gap-4 space-y-4">
-          <img className="rounded-md w-[232px] h-[290px] object-cover" src="./portfolio/client_1.webp" alt="Image 01" />
-          <img className="rounded-md w-[232px] h-[290px] object-cover" src="./portfolio/client_2.webp" alt="Image 02" />
-          <img className="rounded-md w-[232px] h-[174px] object-cover" src="./portfolio/client_3.webp" alt="Image 03" />
-          <img className="rounded-md w-[232px] h-[155px] object-cover" src="./portfolio/client_4.webp" alt="Image 04" />
-          <img className="rounded-md w-[232px] h-[349px] object-cover" src="./portfolio/client_5.webp" alt="Image 05" />
-          <img className="rounded-md w-[232px] h-[250px] object-cover" src="./portfolio/client_6.webp" alt="Image 06" />
-          <img className="rounded-md w-[232px] h-[349px] object-cover" src="./portfolio/client_7.webp" alt="Image 07" />
-          <img className="rounded-md w-[232px] h-[155px] object-cover" src="./portfolio/client_8.webp" alt="Image 08" />
-          <img className="rounded-md w-[232px] h-[250px] object-cover" src="./portfolio/client_9.webp" alt="Image 09" />
-          <img className="rounded-md w-[232px] h-[290px] object-cover" src="./portfolio/client_10.webp" alt="Image 10" />
-          <img className="rounded-md w-[232px] h-[155px] object-cover" src="./portfolio/client_11.webp" alt="Image 11" />
-          <img className="rounded-md w-[232px] h-[309px] object-cover" src="./portfolio/client_12.webp" alt="Image 12" />
+        <div className="mx-auto max-w-140 columns-2 gap-4 space-y-4 p-4 md:max-w-4xl md:columns-4">
+          <img
+            className="h-[290px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_1.webp"
+            alt="Image 01"
+          />
+          <img
+            className="h-[290px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_2.webp"
+            alt="Image 02"
+          />
+          <img
+            className="h-[174px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_3.webp"
+            alt="Image 03"
+          />
+          <img
+            className="h-[155px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_4.webp"
+            alt="Image 04"
+          />
+          <img
+            className="h-[349px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_5.webp"
+            alt="Image 05"
+          />
+          <img
+            className="h-[250px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_6.webp"
+            alt="Image 06"
+          />
+          <img
+            className="h-[349px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_7.webp"
+            alt="Image 07"
+          />
+          <img
+            className="h-[155px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_8.webp"
+            alt="Image 08"
+          />
+          <img
+            className="h-[250px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_9.webp"
+            alt="Image 09"
+          />
+          <img
+            className="h-[290px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_10.webp"
+            alt="Image 10"
+          />
+          <img
+            className="h-[155px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_11.webp"
+            alt="Image 11"
+          />
+          <img
+            className="h-[309px] w-[232px] rounded-md object-cover"
+            src="./portfolio/client_12.webp"
+            alt="Image 12"
+          />
         </div>
       </section>
     </Layout>
