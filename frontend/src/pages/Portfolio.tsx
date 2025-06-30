c05b8f94c5c9fe79b8e73684568b28a23b2c239e
minhtri-dev <minhtritran.dev@gmail.com>
2025-07-01 00:28:35 +1000

    Removed alt

--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -12,62 +12,50 @@ const Portfolio = () => {
           <img
             className="h-[290px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_1.webp"
-            alt="Image 01"
           />
           <img
             className="h-[290px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_2.webp"
-            alt="Image 02"
           />
           <img
             className="h-[174px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_3.webp"
-            alt="Image 03"
           />
           <img
             className="h-[155px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_4.webp"
-            alt="Image 04"
           />
           <img
             className="h-[349px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_5.webp"
-            alt="Image 05"
           />
           <img
             className="h-[250px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_6.webp"
-            alt="Image 06"
           />
           <img
             className="h-[349px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_7.webp"
-            alt="Image 07"
           />
           <img
             className="h-[155px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_8.webp"
-            alt="Image 08"
           />
           <img
             className="h-[250px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_9.webp"
-            alt="Image 09"
           />
           <img
             className="h-[290px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_10.webp"
-            alt="Image 10"
           />
           <img
             className="h-[155px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_11.webp"
-            alt="Image 11"
           />
           <img
             className="h-[309px] w-[232px] rounded-md object-cover"
             src="./portfolio/client_12.webp"
-            alt="Image 12"
           />
         </div>
       </section>
